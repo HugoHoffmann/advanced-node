4ae189e53abab86b6f2fe55d082e07731c36bc76
Hugo Hoffmann <Hugo Hoffmann>
2023-01-12 05:31:43 -0300

    add facebook authentication feature

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,17 @@
+import { AccessToken } from '@/domain/models'
+import { AuthenticationError } from '@/domain/errors'
+
+export interface FacebookAuthentication {
+  perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Return>
+}
+
+namespace FacebookAuthentication {
+  export type Params = {
+    token: string
+  }
+  export type Return = AccessToken | AuthenticationError
+}
+
+
+
+
